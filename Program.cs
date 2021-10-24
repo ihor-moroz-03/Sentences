19a8bcbe30cef6c9aee99058c7ab478127745fce
Ihor Moroz <ihor.moroz.17@gmail.com>
2021-10-24 23:26:08 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FindDeepestNestedBracketsSort
+{
+    class Program
+    {
+        static void Main()
+        {
+            Sentences sentences = new(
+@"Aut (dolores (incidunt)) consequuntur (qui) (((doloribus))) eius sit.
+ Quidem omnis hic sit et porro qui.
+ Recusandae est
+ veniam sed
+ (asperiores) repellendus. Iure repellendus autem sunt.
+ Unde unde omnis nulla laborum. (Et (voluptates omnis sed)) dolorum in eius cupiditate.
+ Cupiditate aut qui dolor.
+ Neque repellat accusantium voluptatem quaerat et velit. Suscipit cum odit eum veniam repellat et suscipit nemo.
+ Quas delectus aliquam fuga dolor iure (voluptatem). Dolorem a modi consectetur quidem dicta dolorem.
+ Qui animi ratione fugit perferendis soluta quia dolor est. Modi est optio quod.
+ Eaque amet dolores facilis impedit. Omnis nobis ad voluptatem.
+ Adipisci sunt nemo beatae illo sed et voluptas beatae.
+ Iste asperiores eum totam. Cupiditate sunt ut ea dicta nemo ab accusamus facere."
+                .Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries));
+
+            foreach (string sent in sentences) Console.WriteLine(sent);
+            Console.WriteLine();
+
+            Console.WriteLine(
+                "Sentence with the greatest amount of nested brackets is\n" +
+                $"[{sentences.GetMaxBrackets()}]\n"
+            );
+
+            Console.WriteLine("Sorted by length:");
+            sentences.Sort((s1, s2) => s1.Length - s2.Length);
+            foreach (string sent in sentences) Console.WriteLine(sent);
+
+            Console.Read();
+        }
+    }
+}
---
+++ Sentences.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindDeepestNestedBracketsSort
+{
+    class Sentences : IEnumerable<string>
+    {
+        List<string> _sentences;
+
+        public Sentences(string[] lines)
+        {
+            _sentences = new();
+            StringBuilder sentence = new();
+            foreach (string line in lines)
+            {
+                if (line.Length == 0) continue;
+                int from = 0, dot;
+                do
+                {
+                    dot = line.IndexOf('.', from);
+                    if (dot == -1)
+                    {
+                        sentence.Append(line[from..]);
+                        break;
+                    }
+                    sentence.Append(line[from..dot]);
+                    from = dot + 1;
+                    _sentences.Add(sentence.ToString());
+                    sentence.Clear();
+                }
+                while (from < line.Length);
+            }
+            //if no dot found in the end, remaining piece is assumed to be a sentence
+            if (sentence.Length > 0) _sentences.Add(sentence.ToString());
+        }
+
+        public IEnumerator<string> GetEnumerator() => _sentences.GetEnumerator();
+
+        public string GetMaxBrackets()
+        {
+            if (_sentences.Count == 0) return "";
+            int max = 0, indexOfMax = 0;
+            for (int i = 0; i < _sentences.Count; ++i)
+            {
+                int count = 0;
+                foreach (char c in _sentences[i])
+                {
+                    switch (c)
+                    {
+                        case '(': count++; break;
+                        case ')':
+                            if (count > max) { max = count; indexOfMax = i; }
+                            count = 0;
+                            break;
+                    }
+                }
+            }
+            return _sentences[indexOfMax];
+        }
+
+        public void Sort(Comparison<string> cmp) => _sentences.Sort(cmp);
+
+        IEnumerator IEnumerable.GetEnumerator() => _sentences.GetEnumerator();
+    }
+}
